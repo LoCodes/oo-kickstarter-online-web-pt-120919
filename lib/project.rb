1d2d277be970f42ccaa51fd5062caf0c59854375
Loren Cipriano <loren25cipriano@gmail.com>
2020-02-01 03:31:23 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,14 +1,19 @@
 class Project
   attr_reader :backers, :title
 
+# Project - ::new
+#  takes a title on initialization, accessible through an attribute reader
+#  initializes with a @backers attribute, an empty array
+
   def initialize(title)
     @title = title
     @backers = []
   end
 
 
-
-# Project - More Advanced #add_backer also adds the project to the backer's backed_projects array
+# Project - #add_backer  accepts a Backer as an argument 
+#           and stores it in a backers array
+#           More Advanced #add_backer also adds the project to the backer's backed_projects array
 
 
   def add_backer(backer)
