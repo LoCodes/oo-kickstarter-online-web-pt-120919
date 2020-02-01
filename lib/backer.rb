edb086d6ed923444259747d3b8556e72389b8003
Loren Cipriano <loren25cipriano@gmail.com>
2020-02-01 03:07:10 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,16 +1,15 @@
 class Backer
-
+      attr_reader :backed_projects, :name
 
 # Backer - ::new
 #  takes a name on initialization, accessible through an attribute reader
 #  initializes with a @backed_projects attribute, an empty array
 
-  attr_reader :backed_projects, :name
-
-  @@backed_projects = []
 
   def initialize(name)
     @name = name
+    @backed_projects = []
+
   end
 
 
